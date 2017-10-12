93a7650df9caecfd9bb5cb8857f2b3b0debb8d8d
Afuwape Ifeoluwa <hipheckt@gmail.com>
2017-10-12 10:29:45 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -161,7 +161,7 @@ angular.module('SongBook', ['ionic', 'song.service'])
 
         if($stateParams.songId === undefined){
             $scope.action = 'Add';
-            $scope.btnaction = 'Save;
+            $scope.btnaction = 'Save';
         }else{
             $scope.action = 'Edit';
             $scope.btnaction = 'Update';
